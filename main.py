32014e992ebbcceb2afcf5822056104cafeb5c3e
Telegin Oleg <teleginos@mail.ru>
2024-09-22 20:03:38 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def get_main_page() -> str:
+    return "Главная страница"
+
+@app.get("/user/admin")
+async def get_admin_page() -> str:
+    return "Вы вошли как администратор"
+
+@app.get("/user/{user_id}")
+async def get_user_number(user_id: str) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
+
+
+@app.get("/user")
+async def get_user_info(username: str, age: int) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
